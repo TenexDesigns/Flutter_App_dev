334b2ba16be1f540484fcc3687eb4af185db9c4e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-05 17:56:45 +0300

    Create 1_Container.java

---
+++ Componennts_or_Widgets/Container/1_Container.java
@@ -0,0 +1,46 @@
+To create a new Flutter app, you can use the `flutter create` command followed by the name of your app. Here are the basic steps:
+
+1. **Open Your Terminal:**
+   - Open your command-line terminal or console. Make sure you have Flutter and Dart installed on your system.
+
+2. **Navigate to Your Desired Project Directory:**
+   - Use the `cd` command to navigate to the directory where you want to create your Flutter app. For example:
+     ```bash
+     cd /path/to/your/desired/directory
+     ```
+
+3. **Create the Flutter App:**
+   - Run the following command to create a new Flutter app:
+     ```bash
+     flutter create your_app_name
+     ```
+     Replace `your_app_name` with the desired name for your app.
+
+4. **Wait for Dependencies to Be Downloaded:**
+   - Flutter will download the necessary dependencies and create the project files. This might take a few minutes depending on your internet connection.
+
+5. **Navigate to the App Directory:**
+   - After the app is created, navigate into the app's directory using the `cd` command:
+     ```bash
+     cd your_app_name
+     ```
+
+6. **Open the Project in Your Preferred IDE:**
+   - Open the newly created Flutter app project in your preferred integrated development environment (IDE) or code editor, such as Android Studio, Visual Studio Code, or IntelliJ IDEA.
+
+7. **Run the App:**
+   - To run the app on an emulator or physical device, use the following command:
+     ```bash
+     flutter run
+     ```
+
+   - Make sure you have an Android emulator, iOS simulator, or a physical device connected and properly configured for testing.
+
+8. **Start Developing:**
+   - You can start developing your Flutter app by editing the Dart code in the `lib/main.dart` file. The `main.dart` file is the entry point of your app.
+
+That's it! You have successfully created a new Flutter app and can now begin developing your Flutter application. You can use the appropriate IDE or code editor to work on your project and customize it as needed.
+
+
+
+  
