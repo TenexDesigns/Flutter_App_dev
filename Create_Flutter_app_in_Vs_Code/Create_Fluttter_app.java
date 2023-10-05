597367ce2ccf3ac6dcae83c6c2435872a1f4ae9f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-05 18:00:21 +0300

    Create Create_Fluttter_app.java

---
+++ Create_Flutter_app_in_Vs_Code/Create_Fluttter_app.java
@@ -0,0 +1,45 @@
+To create a new Flutter app, you can use the following command:
+
+```
+flutter create <project_name>
+```
+
+This will create a new Flutter project in the current directory.
+
+You can also specify the platform that you want to create your app for using the `--platforms` option. For example, to create an app for Android and iOS, you would use the following command:
+
+```
+flutter create <project_name> --platforms android,ios
+```
+
+Once you have created your project, you can build and run it using the following commands:
+
+```
+flutter build
+flutter run
+```
+
+This will build your app and run it on the connected device or emulator.
+
+You can also use the `flutter doctor` command to check that your system is properly configured for Flutter development.
+
+Here is an example of how to create a new Flutter app and run it on an Android device:
+
+1. Open a terminal window and navigate to the directory where you want to create your project.
+2. Run the following command:
+
+```
+flutter create my_app
+```
+
+3. Wait for the project to be created.
+4. Connect your Android device to your computer.
+5. Run the following command:
+
+```
+flutter run
+```
+
+This will build your app and run it on your Android device.
+
+If you are new to Flutter, I recommend that you read the Flutter documentation to learn more about how to get started.
