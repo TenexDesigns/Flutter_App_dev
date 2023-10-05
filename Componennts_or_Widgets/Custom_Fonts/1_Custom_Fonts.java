840c2bb0b512c15df8d9b70bd304879fd265c162
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-05 23:10:21 +0300

    Create 1_Custom_Fonts.java

---
+++ Componennts_or_Widgets/Custom_Fonts/1_Custom_Fonts.java
@@ -0,0 +1,69 @@
+To use custom fonts in a Flutter app, you need to follow these steps:
+
+1. **Download and Place Custom Fonts:**
+   - First, download the custom fonts you want to use in your Flutter app. Make sure the fonts are in a compatible format like TrueType Font (TTF) or OpenType Font (OTF).
+   - Place the font files in a directory within your Flutter project. A common convention is to create a folder named `fonts` at the root level of your project and put the font files there.
+
+2. **Edit the `pubspec.yaml` File:**
+   - Open your project's `pubspec.yaml` file. This file is used to configure your Flutter project, including its dependencies and assets.
+   - Uncomment or add a section for defining your custom fonts. The section you provided in your question is an example of how to define custom fonts in the `pubspec.yaml` file. You should remove the '#' character at the beginning of each line to uncomment the font definitions.
+
+   ```yaml
+   fonts:
+     - family: Schyler
+       fonts:
+         - asset: fonts/Schyler-Regular.ttf
+         - asset: fonts/Schyler-Italic.ttf
+           style: italic
+     - family: Trajan Pro
+       fonts:
+         - asset: fonts/TrajanPro.ttf
+         - asset: fonts/TrajanPro_Bold.ttf
+           weight: 700
+   ```
+
+   - In this example, we have defined two font families: 'Schyler' and 'Trajan Pro'. For each family, we specify the font files and their attributes.
+
+3. **Specify Fonts in Text Widgets:**
+   - To use these custom fonts in your Flutter app, you can specify the font family in `TextStyle` objects within your `Text` widgets.
+
+   ```dart
+   Text(
+     'Custom Font Text',
+     style: TextStyle(
+       fontFamily: 'Schyler', // Use the font family name defined in pubspec.yaml
+       fontSize: 20,
+     ),
+   )
+   ```
+
+   - Make sure to use the same font family name (e.g., 'Schyler') as defined in the `pubspec.yaml` file.
+
+4. **Run `flutter pub get`:**
+   - After making changes to the `pubspec.yaml` file, run the following command to fetch and include the fonts in your project:
+     ```
+     flutter pub get
+     ```
+
+5. **Use the Custom Fonts:**
+   - You can now use the custom fonts in your app's `Text` widgets by specifying the correct `fontFamily` in the `TextStyle` object.
+
+Here's an example of how to use a custom font in a Flutter app:
+
+```dart
+Text(
+  'Custom Font Text',
+  style: TextStyle(
+    fontFamily: 'Schyler', // Use the font family name defined in pubspec.yaml
+    fontSize: 20,
+  ),
+)
+```
+
+This text widget will use the 'Schyler' font family specified in the `pubspec.yaml` file.
+
+The `pubspec.yaml` file is essential for configuring various aspects of your Flutter project, including dependencies, assets, and fonts. By defining custom fonts in this file, you make them available for use in your Flutter app with the specified font family names.
+
+
+
+  ..
