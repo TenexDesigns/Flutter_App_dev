44133a015d201b8686218bbfa7070371d5dd4dc1
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-06 20:26:31 +0300

    Create 2_Routes.java

---
+++ Navigation/Routes/2_Routes.java
@@ -0,0 +1,71 @@
+In Flutter, the `Routes` widget is not used for navigation. Instead, the `Routes` widget is used to define named routes in the `MaterialApp` widget. Named routes provide a way to navigate to different screens in your app using a predefined name.
+
+Here's how you can use the `Routes` widget with code samples:
+
+1. Define Named Routes:
+   - In the `MaterialApp` widget, you can define named routes using the `routes` property.
+   - The `routes` property takes a `Map<String, WidgetBuilder>` where the key is the name of the route and the value is a `WidgetBuilder` function that returns the widget for that route.
+
+   ```dart
+   MaterialApp(
+     routes: {
+       '/home': (context) => HomeScreen(),
+       '/profile': (context) => ProfileScreen(),
+     },
+   )
+   ```
+
+   In the above example, we define two named routes: `/home` and `/profile`. The `HomeScreen` widget is associated with the `/home` route, and the `ProfileScreen` widget is associated with the `/profile` route.
+
+2. Navigate to Named Routes:
+   - To navigate to a named route, you can use the `Navigator.pushNamed()` method.
+   - This method takes a `BuildContext` and the name of the route as parameters.
+
+   ```dart
+   Navigator.pushNamed(context, '/home');
+   ```
+
+   In the above example, calling `Navigator.pushNamed(context, '/home')` will navigate to the `HomeScreen` using the `/home` route.
+
+3. Passing Arguments to Named Routes:
+   - You can also pass arguments to named routes by providing them as parameters to the route's constructor.
+   - To receive the arguments in the destination screen, you can use the `ModalRoute.of(context).settings.arguments` property.
+
+   ```dart
+   Navigator.pushNamed(
+     context,
+     '/profile',
+     arguments: UserProfile(name: 'John', age: 25),
+   );
+   ```
+
+   In the above example, we pass a `UserProfile` object as an argument to the `/profile` route.
+
+   ```dart
+   class ProfileScreen extends StatelessWidget {
+     @override
+     Widget build(BuildContext context) {
+       final UserProfile userProfile =
+           ModalRoute.of(context).settings.arguments as UserProfile;
+
+       return Scaffold(
+         appBar: AppBar(
+           title: Text('Profile'),
+         ),
+         body: Center(
+           child: Text('Name: ${userProfile.name}, Age: ${userProfile.age}'),
+         ),
+       );
+     }
+   }
+   ```
+
+   In the `ProfileScreen`, we retrieve the `UserProfile` object from the route arguments and display its properties.
+
+Named routes are useful when you have a predefined set of screens in your app and want to navigate to them using a specific name. They provide a clean and organized way to handle navigation and make it easier to maintain and modify your app's navigation flow.
+
+I hope this clarifies the usage of the `Routes` widget in Flutter! Let me know if you have any further questions.
+
+
+
+....
