a06bcef91a9ed54dd336e0b6ec5ba54dbb9342d6
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-06 09:50:00 +0300

    Update 1_Images.java

--- Componennts_or_Widgets/Images/1_Images.java
+++ Componennts_or_Widgets/Images/1_Images.java
@@ -67,6 +67,82 @@ Image.asset(
 
 In summary, to display images in Flutter, you can use the Image widget and specify the source of the image using the Image.network(), Image.asset(), or Image.file() constructor. You can also customize the appearance of the images using various properties of the Image widget.
 
+
+
+
+To display images in Flutter, you can use the following widgets:
+
+Image: This widget displays a local image.
+NetworkImage: This widget displays an image from the network.
+Image widget
+
+The Image widget has the following parameters:
+
+image: This parameter specifies the image to be displayed.
+width: This parameter specifies the width of the image.
+height: This parameter specifies the height of the image.
+fit: This parameter specifies how the image should be scaled to fit its container.
+alignment: This parameter specifies the alignment of the image within its container.
+NetworkImage widget
+
+The NetworkImage widget has the following parameters:
+
+src: This parameter specifies the URL of the image to be displayed.
+width: This parameter specifies the width of the image.
+height: This parameter specifies the height of the image.
+fit: This parameter specifies how the image should be scaled to fit its container.
+alignment: This parameter specifies the alignment of the image within its container.
+
+
+
+
+
+  Styling Images
+You can style an image in Flutter using the BoxDecoration widget for a Container. Here's an example of how to set the border radius and shadow of an image:
+
+Container(
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(10),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withOpacity(0.5),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: Offset(0, 3),
+      ),
+    ],
+    image: DecorationImage(
+      image: AssetImage('assets/my_image.jpg'),
+      fit: BoxFit.cover,
+    ),
+  ),
+)
+In this example, BoxDecoration is used to create a rounded image with a shadow. The BoxDecoration widget contains a borderRadius property to set the border radius, a boxShadow property to set the shadow, and an image property to set the image. The fit property of DecorationImage is used to determine how to inscribe the image into the box medium.com dev.to.
+
+Additional Notes
+Remember to run flutter pub get in your terminal to update your project's dependencies after editing the pubspec.yaml file.
+If you're working with many images or large images, you might encounter memory issues. In this case, consider using a package like cached_network_image or flutter_advanced_networkimage for caching and better memory management.
+For images from the network, you may want to handle the case where the image fails to load. You can do this by using the FadeInImage widget and providing a placeholder image that is displayed while the network image is loading docs.flutter.dev.
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 Citations:
 [1] https://docs.flutter.dev/cookbook/images/network-image
 [2] https://docs.flutter.dev/ui/assets/assets-and-images
