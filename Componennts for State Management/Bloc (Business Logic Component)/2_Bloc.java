273645a2c4c6f06f65f47825f7bd133747ef2d6f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-06 16:21:05 +0300

    Create 2_Bloc.java

---
+++ Componennts for State Management/Bloc (Business Logic Component)/2_Bloc.java
@@ -0,0 +1,78 @@
+The Business Logic Component (BLoC) pattern is a popular design pattern in Flutter that separates business logic from the user interface, making it easier to manage state and build modular, testable code [Source 0](https://www.flutterclutter.dev/flutter/basics/what-is-the-bloc-pattern/2021/2084/), [Source 1](https://blog.logrocket.com/state-management-flutter-bloc-pattern/).
+
+Here are the main components involved in the BLoC pattern:
+
+- **Events**: These are inputs to the BLoC, such as user interactions like button presses.
+- **States**: These are outputs from the BLoC. They represent parts of your app's state.
+- **BLoC**: This is where the business logic sits. It takes events as input, processes them, and produces states as output.
+
+Here's an example of how to implement the BLoC pattern:
+
+```dart
+import 'package:bloc/bloc.dart';
+
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+    }
+  }
+}
+```
+
+In this example, we create a `CounterBloc` that takes `CounterEvent` as input and outputs `int` as state. The `mapEventToState` method is where we define the business logic: when an increment event is received, the state is incremented by 1; when a decrement event is received, the state is decremented by 1.
+
+To use this BLoC in the UI, you can use a `BlocBuilder` widget to listen to the state and rebuild the UI whenever the state changes:
+
+```dart
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (context, count) {
+          return Center(child: Text('$count'));
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => context.read<CounterBloc>().add(CounterEvent.increment),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            child: Icon(Icons.remove),
+            onPressed: () => context.read<CounterBloc>().add(CounterEvent.decrement),
+          ),
+        ],
+      ),
+    );
+  }
+}
+```
+
+In this example, we use `BlocBuilder` to rebuild the `Center` widget whenever the counter changes. The `FloatingActionButton` widgets dispatch increment and decrement events when pressed.
+
+The BLoC pattern can be used in various parts of your Flutter application. It is especially useful when you need to share state between different parts of your app, or when you have complex business logic that you want to keep separate from the UI. It can also make your code more testable and reusable [Source 1](https://blog.logrocket.com/state-management-flutter-bloc-pattern/), [Source 3](https://pub.dev/packages/flutter_bloc), [Source 5](https://dzone.com/articles/guide-to-implementing-bloc-architecture-in-flutter), [Source 12](https://www.geeksforgeeks.org/how-to-manage-state-in-flutter-with-bloc-pattern/).
+
+
+
+
+
+....
