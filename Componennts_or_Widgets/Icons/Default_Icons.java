8eaec492ef8a95ab7247afb5cff8dbda6e842fc0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-06 11:42:20 +0300

    Create Default_Icons.java

---
+++ Componennts_or_Widgets/Icons/Default_Icons.java
@@ -0,0 +1,88 @@
+Flutter provides a variety of built-in icons that you can use in your Flutter applications. These icons are part of the `Icons` class in the `flutter/material.dart` library. Here are some commonly used built-in icons and how to use them with code samples:
+
+
+
+
+
+The website
+
+https://fonts.google.com/icons
+
+
+1. **Arrow Icons:**
+
+   - `Icons.arrow_back`: A left-pointing arrow.
+   - `Icons.arrow_forward`: A right-pointing arrow.
+
+   ```dart
+   Icon(Icons.arrow_back) // Left arrow icon
+   Icon(Icons.arrow_forward) // Right arrow icon
+   ```
+
+2. **Action Icons:**
+
+   - `Icons.search`: A search icon.
+   - `Icons.add`: A plus/add icon.
+   - `Icons.delete`: A delete/trash icon.
+   - `Icons.refresh`: A refresh icon.
+   
+   ```dart
+   Icon(Icons.search) // Search icon
+   Icon(Icons.add) // Plus icon
+   Icon(Icons.delete) // Delete icon
+   Icon(Icons.refresh) // Refresh icon
+   ```
+
+3. **Navigation Icons:**
+
+   - `Icons.menu`: A menu/hamburger icon.
+   - `Icons.home`: A home icon.
+   - `Icons.settings`: A settings/gear icon.
+   - `Icons.notifications`: A notifications/bell icon.
+
+   ```dart
+   Icon(Icons.menu) // Menu icon
+   Icon(Icons.home) // Home icon
+   Icon(Icons.settings) // Settings icon
+   Icon(Icons.notifications) // Notifications icon
+   ```
+
+4. **Social Media Icons:**
+
+   - `Icons.facebook`: Facebook logo.
+   - `Icons.twitter`: Twitter logo.
+   - `Icons.instagram`: Instagram logo.
+   - `Icons.youtube`: YouTube logo.
+
+   ```dart
+   Icon(Icons.facebook) // Facebook icon
+   Icon(Icons.twitter) // Twitter icon
+   Icon(Icons.instagram) // Instagram icon
+   Icon(Icons.youtube) // YouTube icon
+   ```
+
+5. **Communication Icons:**
+
+   - `Icons.email`: An email/envelope icon.
+   - `Icons.phone`: A phone/call icon.
+   - `Icons.chat_bubble`: A chat message bubble icon.
+
+   ```dart
+   Icon(Icons.email) // Email icon
+   Icon(Icons.phone) // Phone icon
+   Icon(Icons.chat_bubble) // Chat bubble icon
+   ```
+
+6. **Media Icons:**
+
+   - `Icons.camera`: A camera icon.
+   - `Icons.music_note`: A music note icon.
+   - `Icons.movie`: A movie/film icon.
+
+   ```dart
+   Icon(Icons.camera) // Camera icon
+   Icon(Icons.music_note) // Music note icon
+   Icon(Icons.movie) // Movie icon
+   ```
+
+These are just a few examples of the built-in icons available in Flutter. To use these icons, simply create an `Icon` widget and pass the desired `Icons` constant as its argument, as shown in the code samples above. You can customize the icon's size and color using the `size` and `color` parameters of the `Icon` widget.
