6b2a6fdd734a3b19d92561c90872f5eb7cecf7eb
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-09 15:10:47 +0300

    Create 2_Table.java

---
+++ Componennts_or_Widgets/Table/2_Table.java
@@ -0,0 +1,91 @@
+In Flutter, you can create tables using the `Table` widget. The `Table` widget allows you to display data in rows and columns, similar to an HTML table. Here's how you can create a simple table in Flutter with code samples:
+
+```dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Table Example'),
+        ),
+        body: Center(
+          child: MyTable(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyTable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      border: TableBorder.all(),
+      defaultColumnWidth: FixedColumnWidth(100.0),
+      children: [
+        TableRow(
+          children: [
+            TableCell(
+              child: Center(child: Text('Header 1')),
+            ),
+            TableCell(
+              child: Center(child: Text('Header 2')),
+            ),
+            TableCell(
+              child: Center(child: Text('Header 3')),
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            TableCell(
+              child: Center(child: Text('Data 1')),
+            ),
+            TableCell(
+              child: Center(child: Text('Data 2')),
+            ),
+            TableCell(
+              child: Center(child: Text('Data 3')),
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            TableCell(
+              child: Center(child: Text('Data 4')),
+            ),
+            TableCell(
+              child: Center(child: Text('Data 5')),
+            ),
+            TableCell(
+              child: Center(child: Text('Data 6')),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+```
+
+In this example:
+
+1. We create a Flutter app with a simple table layout.
+2. The `Table` widget is used to define the table structure.
+3. We set the `border` property to create borders around the table and its cells.
+4. The `defaultColumnWidth` property specifies the default width for all columns.
+5. Inside the `Table`, we define rows using the `TableRow` widget, and each row contains `TableCell` widgets.
+6. You can customize the content of each cell with various widgets as needed.
+
+This example demonstrates a basic table with headers and data cells. You can further customize the appearance and content of your table based on your specific requirements.
+
+
+
+....
