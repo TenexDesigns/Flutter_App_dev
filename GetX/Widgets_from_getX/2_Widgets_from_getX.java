917991ef81e5da4a9874823036fceda9c1933269
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-09 16:09:07 +0300

    Create 2_Widgets_from_getX.java

---
+++ GetX/Widgets_from_getX/2_Widgets_from_getX.java
@@ -0,0 +1,23 @@
+In the GetX package, there are several widgets that provide additional functionality and convenience for state management, dependency injection, and navigation in Flutter. Here are some of the commonly used widgets from the GetX package:
+
+1. **GetBuilder**: The `GetBuilder` widget is used for reactive state management. It rebuilds the widget whenever the specified controller's state changes. It is a lightweight alternative to `Obx` and is useful when you only need to update a specific part of the UI.
+
+2. **GetX**: The `GetX` widget is used for reactive state management and dependency injection. It combines the functionality of `GetBuilder` and `Get.put` by automatically injecting the specified controller into the widget tree and rebuilding the widget whenever the controller's state changes.
+
+3. **GetBuilderOr**: The `GetBuilderOr` widget is similar to `GetBuilder`, but it allows you to specify a fallback widget to display when the controller is not yet ready or when an error occurs.
+
+4. **GetView**: The `GetView` widget is used to bind a specific controller to a widget. It automatically injects the specified controller into the widget tree and rebuilds the widget whenever the controller's state changes.
+
+5. **GetWidget**: The `GetWidget` widget is similar to `GetView`, but it does not rebuild the widget when the controller's state changes. It is useful when you only need to access the controller's properties or methods without triggering a rebuild.
+
+6. **GetMaterialApp**: The `GetMaterialApp` widget is a replacement for the default `MaterialApp` widget provided by Flutter. It sets up the necessary dependencies for GetX, such as the routing system and dependency injection.
+
+7. **GetPageRoute**: The `GetPageRoute` widget is used to define a named route in GetX. It allows you to specify the route's name, the widget to be displayed, and any additional parameters or bindings.
+
+These widgets from the GetX package provide a convenient and efficient way to manage state, inject dependencies, and navigate between screens in Flutter. They help simplify the code and improve the overall development experience by reducing boilerplate and improving code organization.
+
+
+
+
+
+  ....
