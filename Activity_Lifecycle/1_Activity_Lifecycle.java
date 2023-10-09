04c25bdb6dc42b6d4cc26b8c4b419735e779dee9
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-09 11:05:18 +0300

    Update 1_Activity_Lifecycle.java

--- Activity_Lifecycle/1_Activity_Lifecycle.java
+++ Activity_Lifecycle/1_Activity_Lifecycle.java
@@ -43,7 +43,25 @@ In this code, `WidgetsBindingObserver` is being used to listen to the app lifecy
 
 In conclusion, `BuildContext` and app lifecycle are important concepts in Flutter. Understanding them can help you build more efficient and responsive apps.
 
+     ```
 
+     Here, `Theme.of(context).primaryColor` accesses the primary color from the current theme based on the context.
+
+4. **Application Lifecycle in Flutter:**
+   - Flutter apps do have a lifecycle, but it's not as complex as Android's activity lifecycle.
+   - Flutter's app lifecycle consists of methods like `main`, `runApp`, and the `StatefulWidget` lifecycle methods (`initState`, `build`, `dispose`).
+   - Flutter widgets respond to changes and updates, and they are rebuilt when needed.
+
+5. **Widget Lifecycle vs. Activity Lifecycle:**
+   - Flutter widgets have their own lifecycle, where they are built, updated, and disposed of as necessary.
+   - It's different from Android's activity lifecycle, which is more focused on the entire application and UI components.
+
+6. **Widget Rebuilding:**
+   - Widgets are rebuilt when their state changes or when their parent widget is rebuilt.
+   - `BuildContext` is often used to build a new widget tree within the `build` method when state changes.
+
+
+  
 
 
 
