29897c4e4f3b115f6575a8f96d28ae196a6d9cfa
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-10 21:45:54 +0300

    Update Serialization_and_Desirialization.java

--- Http_Api_calls/Serialization_and_Desirialization.java
+++ Http_Api_calls/Serialization_and_Desirialization.java
@@ -1,3 +1,92 @@
+class Post {
+  final int id;
+  final String title;
+  final String body;
+
+  Post({
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {  // This is used to extract data from the Ma objects returned from the api and convret them into post objects the return them where Post.fromjson is callded
+    return Post(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+
+----------------------------------------------------------------------------------------------------------
+  Future<List<Post>> fetchPosts() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    final List<dynamic> jsonData = json.decode(response.body);
+    
+    return jsonData.map((json) => Post.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load data from the API');
+  }
+}
+
+
+Here is where Post.json is called and the map objects are passed, then the Maps objects received are convered to  post Objects , then return and themn are pit into a list of Posts
+
+
+Note, this is the json data from the api "{
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },"
+
+and it is converted to a Map object in fluttter on line  List<dynamic> jsonData = json.decode(response.body);, then there map objecrs are passed to Post.from using map and then convert them to Post objects
+
+
+----------------------------------------------------------------------------------------------------------
+
+In the context of HTTP API calls, the code you provided defines a Dart class called `Post` that represents a data model for objects you expect to receive from the API. Let's break down the key components of this code:
+
+1. `class Post { ... }`: This is the definition of the `Post` class. It represents the structure of the data you expect to receive from the API. It has three fields: `id`, `title`, and `body`, each representing a property of the `Post` object.
+
+2. `required`: The `required` keyword is used in the constructor parameters (e.g., `required this.id`) to indicate that these fields must be provided when creating an instance of the `Post` class. In other words, they are required properties, and you cannot create a `Post` object without providing values for these properties.
+
+3. `factory Post.fromJson(Map<String, dynamic> json) { ... }`: This is a factory constructor within the `Post` class. A factory constructor is a special type of constructor that is responsible for creating instances of the class. In this case, it's used to create a `Post` object from a JSON map (`Map<String, dynamic>`). It takes a `json` parameter, which is the JSON data received from the API.
+
+   - Inside the `fromJson` factory constructor, it extracts data from the `json` map using keys (e.g., `json['id']`, `json['title']`, `json['body']`) and uses that data to create a `Post` object. It then returns the created object.
+
+   - The purpose of this factory constructor is to encapsulate the logic required to convert JSON data into a Dart object, making it easier to work with API responses.
+
+4. `Post.fromJson(json)).toList();`: This line of code is part of the data processing logic when receiving data from an API. It takes a JSON response and converts it into a list of `Post` objects.
+
+   - `json` is typically the JSON data received from the API. It represents an array of objects, each containing properties like `id`, `title`, and `body`.
+
+   - `json.map((json) => Post.fromJson(json)).toList();` uses the `map` function to iterate over each JSON object in the array. For each JSON object, it calls the `Post.fromJson(json)` factory constructor to create a `Post` object. The `map` function transforms the JSON objects into a list of `Post` objects.
+
+So, `Post.fromJson(json)` takes a single JSON object and converts it into a `Post` object, while `json.map((json) => Post.fromJson(json)).toList();` takes a JSON array (list of objects) and converts it into a list of `Post` objects. This is a common pattern when dealing with API responses: converting JSON data into structured Dart objects for easier manipulation in your Flutter application.
+
+
+
+
+
+
+  .......
+
+
+
+
+
+
+
+
+
+
+
+
+
 The methods like `.fromJson` and `.toList` that you've seen in the context of parsing JSON data in Flutter are not built-in methods provided by Flutter or Dart. Instead, they are typically custom methods that you define in your Dart classes to help with serialization and deserialization of data when working with JSON.
 
 Here's a brief explanation of these methods:
