427fad22fc65926a6525046fdf047a8198b477a2
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-10 15:06:20 +0300

    Create 1_Named_route_naviagtion.java

---
+++ GetX/Navigation/1_Named_route_naviagtion.java
@@ -0,0 +1,78 @@
+In Flutter with the GetX package, you can use named routes for route navigation. Named routes provide a convenient way to navigate between different screens or pages in your app. Here's how to set up and use named routes with GetX:
+
+1. **Define Your Named Routes**:
+
+   First, define your named routes in the main application file or wherever you set up your app. Typically, this is done in the `MaterialApp` widget's `routes` parameter.
+
+   ```dart
+   import 'package:flutter/material.dart';
+   import 'package:get/get.dart';
+
+   void main() {
+     runApp(MyApp());
+   }
+
+   class MyApp extends StatelessWidget {
+     @override
+     Widget build(BuildContext context) {
+       return GetMaterialApp(
+         title: 'My App',
+         initialRoute: '/', // Set the initial route
+         getPages: [
+           GetPage(name: '/', page: () => HomeScreen()), // Home Screen
+           GetPage(name: '/details', page: () => DetailsScreen()), // Details Screen
+           // Add more routes as needed
+         ],
+       );
+     }
+   }
+   ```
+
+2. **Navigate to Named Routes**:
+
+   To navigate to a named route, you can use the `Get.toNamed` method. For example, when a button is pressed, you can navigate to the "details" route:
+
+   ```dart
+   ElevatedButton(
+     onPressed: () {
+       Get.toNamed('/details'); // Navigate to the "details" route
+     },
+     child: Text('Go to Details'),
+   ),
+   ```
+
+   You can also pass data to the new route by specifying arguments:
+
+   ```dart
+   Get.toNamed('/details', arguments: {'id': 123}); // Pass arguments
+   ```
+
+3. **Access Route Arguments**:
+
+   In the destination route, you can access the arguments passed to it using GetX's `Get.arguments`. For example, in the "details" route:
+
+   ```dart
+   class DetailsScreen extends StatelessWidget {
+     @override
+     Widget build(BuildContext context) {
+       final arguments = Get.arguments;
+       final id = arguments['id'];
+
+       return Scaffold(
+         appBar: AppBar(
+           title: Text('Details Screen'),
+         ),
+         body: Center(
+           child: Text('Item ID: $id'),
+         ),
+       );
+     }
+   }
+   ```
+
+   By using named routes and the `Get.arguments` property, you can easily navigate between different screens in your Flutter app with GetX.
+
+
+
+
+     ..
