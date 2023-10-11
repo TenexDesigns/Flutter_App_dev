f4be13bf96cc3ed3d45febfdc143eae292d607b6
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-11 21:29:59 +0300

    Create 2_Flexible_Align_Sizeable.java

---
+++ Componennts_or_Widgets/Flexible_Align_Sizeable/2_Flexible_Align_Sizeable.java
@@ -0,0 +1,51 @@
+`Flexible`, `SizedBox`, and `Align` are useful Flutter widgets for creating flexible layouts, adding spacing, and aligning child widgets. Here's an explanation and code samples for each:
+
+1. `Flexible` Widget:
+   - The `Flexible` widget is used to create flexible space within a `Row`, `Column`, or `Flex` layout. It allows a child widget to take up a portion of the available space.
+   - The `flex` property specifies how the space is distributed among child widgets within the same `Flex` parent.
+   
+   Code Sample:
+   ```dart
+   Row(
+     children: <Widget>[
+       Flexible(
+         flex: 2,  // Takes twice as much space as the second Flexible
+         child: Container(color: Colors.red),
+       ),
+       Flexible(
+         flex: 1,
+         child: Container(color: Colors.blue),
+       ),
+     ],
+   )
+   ```
+
+2. `SizedBox` Widget:
+   - The `SizedBox` widget is used to create a box of fixed dimensions (width and height).
+   - It's helpful for adding spacing or setting a minimum or maximum size for other widgets.
+
+   Code Sample:
+   ```dart
+   SizedBox(
+     width: 100.0,
+     height: 50.0,
+     child: RaisedButton(
+       onPressed: () {},
+       child: Text('Click Me'),
+     ),
+   )
+   ```
+
+3. `Align` Widget:
+   - The `Align` widget is used to align its child within a parent widget.
+   - You can specify the alignment using properties like `alignment` (alignment within the parent) and `child` (the widget to be aligned).
+
+   Code Sample:
+   ```dart
+   Align(
+     alignment: Alignment.center,
+     child: Text('Centered Text'),
+   )
+   ```
+
+You can use these widgets to create flexible and organized layouts in your Flutter app. They are especially useful when working with `Row`, `Column`, and other layout widgets to control how widgets are displayed within them and provide the desired spacing and alignment.
