5e02575a2f0376744c775976d9ebffc9f99de042
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-11 21:19:59 +0300

    Create 1_FutureBuilder.java

---
+++ Componennts_or_Widgets/FutureBuilder/1_FutureBuilder.java
@@ -0,0 +1,68 @@
+The FutureBuilder widget in Flutter allows you to build a widget based on the latest snapshot of a Future. It is a convenient way to handle asynchronous operations in your Flutter apps.
+
+**Parameters**
+
+The FutureBuilder widget has the following parameters:
+
+* **future:** The Future to listen to.
+* **initialData:** The initial data to display until the Future completes.
+* **builder:** A callback that builds a widget based on the latest snapshot of the Future.
+
+**When to use**
+
+You should use the FutureBuilder widget when you need to display a widget based on the latest snapshot of a Future. This can be useful for a variety of tasks, such as:
+
+* Displaying data that is loaded asynchronously from a network or database.
+* Displaying a loading indicator while asynchronous operations are in progress.
+* Displaying an error message if an asynchronous operation fails.
+
+**Code samples**
+
+The following code sample shows how to use the FutureBuilder widget to display a list of items that are loaded asynchronously from a network:
+
+```dart
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final items = Future.delayed(Duration(seconds: 1), () => ['Item 1', 'Item 2', 'Item 3']);
+
+    return MaterialApp(
+      home: Scaffold(
+        body: FutureBuilder(
+          future: items,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final itemsList = snapshot.data as List<String>;
+
+              return ListView.builder(
+                itemCount: itemsList.length,
+                itemBuilder: (context, index) {
+                  return Text(itemsList[index]);
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error loading items: ${snapshot.error}');
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+```
+
+This code will display a loading indicator while the Future is in progress. Once the Future completes, the list of items will be displayed. If the Future fails, an error message will be displayed.
+
+**Conclusion**
+
+The FutureBuilder widget is a convenient way to handle asynchronous operations in your Flutter apps. It is an essential tool for any Flutter developer.
+
+
+
+
+
+  .....
