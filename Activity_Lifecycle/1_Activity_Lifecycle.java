03ee6d6542f80fd40f59306ed0c9ec7f8ee6d3ea
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-12 15:00:40 +0300

    Update 1_Activity_Lifecycle.java

--- Activity_Lifecycle/1_Activity_Lifecycle.java
+++ Activity_Lifecycle/1_Activity_Lifecycle.java
@@ -1,8 +1,12 @@
-As for app lifecycle, Flutter doesn't have an exact equivalent to Android's Activity lifecycle or iOS's ViewController lifecycle. However, it does have widget lifecycle callbacks that allow you to manage resources. 
+As for app lifecycle, Flutter doesn't have an exact equivalent to Android's Activity lifecycle or iOS's ViewController lifecycle.
+  However, it does have widget lifecycle callbacks that allow you to manage resources. 
 
-For instance, `StatefulWidget` has lifecycle methods like `initState`, `didUpdateWidget`, `dispose`, etc. `initState` is called once when the object is inserted into the tree, `didUpdateWidget` is called whenever the widget configuration changes, and `dispose` is called when the `State` object is removed from the tree permanently [Source 26](https://www.geeksforgeeks.org/life-cycle-of-flutter-widgets/).
+For instance, `StatefulWidget` has lifecycle methods like `initState`, `didUpdateWidget`, `dispose`, etc. `initState` is called once when the object 
+  is inserted into the tree, `didUpdateWidget` is called whenever the widget configuration changes, and `dispose` is called when the `State` object is
+  removed from the tree permanently [Source 26](https://www.geeksforgeeks.org/life-cycle-of-flutter-widgets/).
 
-Flutter also provides an `AppLifecycleState` enum to handle app state changes. The states are `paused`, `resumed`, `inactive`, and `suspending` [Source 17](https://medium.com/gytworkz/deep-dive-into-flutter-app-lifecycle-342b797480aa). 
+Flutter also provides an `AppLifecycleState` enum to handle app state changes.
+  The states are `paused`, `resumed`, `inactive`, and `suspending` [Source 17](https://medium.com/gytworkz/deep-dive-into-flutter-app-lifecycle-342b797480aa). 
 
 You can listen to these state changes by adding a `WidgetsBindingObserver` to your `StatefulWidget`:
 
